f954d676dd78b723f5ad1e83628bcf21fa6492ad
Jasmine Li <jasminel.lyr@gmail.com>
2022-01-20 20:53:02 -0800

    image dim

--- timm/data/dataset.py
+++ timm/data/dataset.py
@@ -189,8 +189,8 @@ class SalienceImageDataset(ImageDataset):
 
     def __getitem__(self, index):
         img, target = super().__getitem__(index)
-        if img.shape == (6, 4, self.large_size, self.large_size):
-            return img, target
+        # if img.shape == (6, 4, self.large_size, self.large_size):
+        #     return img, target
 
         trans = transforms.ToPILImage()
         img = trans(img)
